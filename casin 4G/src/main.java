c7595ca8bea52e59b3a52ad05638ad0d3528f74d
Cristian <Cristian@CriPC.station>
2020-11-20 12:24:49 +0100

    main

--- casin 4G/src/main.java
+++ casin 4G/src/main.java
@@ -1,4 +1,30 @@
+/* Progetto del casinò della 4G
+ * Programmatori: Cristian Lorenti, Delmonte daniele ....
+ * */
 
+import java.util.Scanner; //non lo so sincero lo fa sempre il prof
 public class main {
-
-}
+ public static void main(String[] args) {
+	 
+	 int sceltaGiochi=0;
+	 
+	 System.out.println("Benvenuti nel casinò della 4G"); // stampa del benvenuto 
+	 System.out.println("a che gioco vuoi giocare?"); 
+	 System.out.println(""); //menu per la selezione dei giochi
+	 
+	 switch (sceltaGiochi) {
+	 
+	 case 0:
+		 break;
+	 case 1:
+		 break;
+	 case 2:
+		 break;
+		 
+	 
+	 } //switch della scielta dei giochi  /*delmo puoi aggiungere qualche idea per i giochi da casinò*/
+	 
+	 
+	
+ } //main 
+} //main class 
