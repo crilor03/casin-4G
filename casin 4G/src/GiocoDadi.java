4483e5cb158d59fc01b828c0454a0bc79a72047e
Cristian <Cristian@CriPC.station>
2020-11-20 12:41:50 +0100

    aggiunta gioco dadi

---
+++ casin 4G/src/GiocoDadi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class GiocoDadi {
+   public static void GiocoDADI () { 
+	   int sceltaRegole=0; //dichiarazione scelta dell'utente Regole
+	   
+		System.out.println("hai scelto il gioco dei dadi");
+		System.out.println("vuoi sapere le regole del gioco?");
+		
+		
+   }//GiocoDADI
+	
+} //GiocoDadi class
--- casin 4G/src/main.java
+++ casin 4G/src/main.java
@@ -6,15 +6,18 @@ import java.util.Scanner; //non lo so sincero lo fa sempre il prof
 public class main {
  public static void main(String[] args) {
 	 
-	 int sceltaGiochi=0;
+	 Scanner sc = new Scanner(System.in); //dichiarazione dello scanner 
+	 int sceltaGiochi=0; //dichiarzione variabile per la scelta dell'utente
 	 
 	 System.out.println("Benvenuti nel casinò della 4G"); // stampa del benvenuto 
 	 System.out.println("a che gioco vuoi giocare?"); 
 	 System.out.println(""); //menu per la selezione dei giochi
 	 
+	 sceltaGiochi = sc.nextInt();
 	 switch (sceltaGiochi) {
 	 
 	 case 0:
+		GiocoDadi.GiocoDADI();
 		 break;
 	 case 1:
 		 break;
